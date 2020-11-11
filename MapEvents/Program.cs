8183abc28f0702006e47fdc48761e4aa85f62f1e
Pedro Dias <pedro@dolittle.com>
2020-11-11 06:00:42 +0100

    Initial new project

---
+++ MapEvents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
